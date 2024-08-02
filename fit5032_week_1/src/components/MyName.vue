a62d229c0d15dd77a2356669af50bf05978aaaca
Venture <whenchange.shi@gmail.com>
2024-08-02 22:58:34 +1000

    first commit

---
+++ fit5032_week_1/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+  <div class="myName">
+    <h1>Hello, I am Wencheng Shi</h1>
+  </div>
+</template>
+
+<style scoped>
+.myName {
+  padding-top: 10px;
+}
+h1 {
+  color: #42b983;
+}
+</style>
