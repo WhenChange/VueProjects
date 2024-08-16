3f6bf1548df157afe590d0470f53d574fe79ea65
Venture <whenchange.shi@gmail.com>
2024-08-16 15:10:44 +1000

    Finished activity 3 for lab 4.

--- week-4/src/components/InputForm.vue
+++ week-4/src/components/InputForm.vue
@@ -35,20 +35,41 @@
             <!-- <div class="col-md-6"> -->
             <div class="col-6 col-sm-6">
               <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username" required v-model="formData.username" />
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
+                v-model="formData.username"
+              />
+
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
             <!-- <div class="col-md-6"> -->
             <div class="col-6 col-sm-6">
               <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" id="password" minlength="4" maxlength="10"
-                v-model="formData.password" />
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
+                v-model="formData.password"
+              />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
           </div>
           <div class="row mb-3">
             <!-- <div class="col-md-6"> -->
             <div class="col-6 col-sm-6">
               <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian" />
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="isAustralian"
+                  v-model="formData.isAustralian"
+                />
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
@@ -66,16 +87,31 @@
             <!-- <div class="col-md-6"> -->
             <div class="col-6 col-sm-6">
               <label for="gender" class="form-label">Gender</label>
-              <select class="form-select" id="gender" required v-model="formData.gender">
+              <select
+                class="form-select"
+                id="gender"
+                v-model="formData.gender"
+                @blur="() => validateGender(true)"
+                @input="() => validateGender(false)"
+              >
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
+              <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
             </div>
           </div>
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label>
-            <textarea class="form-control" id="reason" rows="3" required v-model="formData.reason"></textarea>
+            <textarea
+              class="form-control"
+              id="reason"
+              rows="3"
+              v-model="formData.reason"
+              @blur="() => validateReason(true)"
+              @input="() => validateReason(false)"
+            ></textarea>
+            <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -88,7 +124,12 @@
   <!-- new -->
   <div class="row mt-5" v-if="submittedCards.length">
     <div class="d-flex flex-wrap justify-content-start">
-      <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem">
+      <div
+        v-for="(card, index) in submittedCards"
+        :key="index"
+        class="card m-2"
+        style="width: 18rem"
+      >
         <div class="card-header">User Information</div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">Username: {{ card.username }}</li>
@@ -119,10 +160,27 @@ const formData = ref({
 const submittedCards = ref([])
 
 const submitForm = () => {
-  submittedCards.value.push({
-    ...formData.value
-  })
+  validateName(true)
+  validatePassword(true)
+  validateGender(true)
+  validateReason(true)
+
+  if (
+    !errors.value.username &&
+    !errors.value.password &&
+    !errors.value.gender &&
+    !errors.value.reason
+  ) {
+    submittedCards.value.push({ ...formData.value })
+    // clearForm()
+  }
 }
+
+// const submitForm = () => {
+//   submittedCards.value.push({
+//     ...formData.value
+//   })
+// }
 
 const clearForm = () => {
   // Default Form
@@ -134,7 +192,63 @@ const clearForm = () => {
     gender: ''
   }
   // Clear Form
-  submittedCards.value = []
+  // submittedCards.value = []
+}
+
+const errors = ref({
+  username: null,
+  password: null,
+  resident: null,
+  gender: null,
+  reason: null
+})
+
+const validateName = (blur) => {
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
+}
+
+const validatePassword = (blur) => {
+  const password = formData.value.password
+  const minLength = 8
+  const hasUppercase = /[A-Z]/.test(password)
+  const hasLowercase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+
+  if (password.length < minLength) {
+    if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = 'Password must contain at least one number.'
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = 'Password must contain at least one special character.'
+  } else {
+    errors.value.password = null
+  }
+}
+
+const validateGender = (blur) => {
+  const validOptions = ['male', 'female', 'other']
+  if (!validOptions.includes(formData.value.gender)) {
+    if (blur) errors.value.gender = 'Gender must be one of the three options.'
+  } else {
+    errors.value.gender = null
+  }
+}
+
+const validateReason = (blur) => {
+  if (!formData.value.reason) {
+    if (blur) errors.value.reason = 'Reason can not be empty.'
+  } else {
+    errors.value.reason = null
+  }
 }
 </script>
 
