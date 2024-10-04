ace48d858ee18a54c2465ddaca22e3a4653fa7ae
Venture <whenchange.shi@gmail.com>
2024-10-04 10:53:54 +1000

    Restart the Lab 9.

---
+++ week-9/src/router/index.js
@@ -0,0 +1,85 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import LoginView from '../views/LoginView.vue'
+import RedirectView from '../views/RedirectView.vue'
+import store from '../store/store'
+import FirebaseSigninView from '../views/FirebaseSigninView.vue'
+import FirebaseRegisterView from '../views/FirebaseRegisterView.vue'
+import AddBookView from '../views/AddBookView.vue'
+import SearchBookView from '../views/SearchBookView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/addbook',
+    name: 'AddBook',
+    component: AddBookView
+  },
+  {
+    path: '/searchbook',
+    name: 'SearchBook',
+    component: SearchBookView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView,
+    // Per-route Guard
+    // beforeEnter: (to, from) => {
+    //   return false
+    // }
+    beforeEnter: (to, from, next) => {
+      if (!store.state.isAuthenticated) {
+        next({ name: 'Redirect' });
+      } else {
+        next(); 
+      }
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
+  },
+  {
+    path: '/redirect',
+    name: 'Redirect',
+    component: RedirectView
+  },
+  {
+    path: '/FireLogin',
+    name: 'FireLogin',
+    component: FirebaseSigninView,
+  },
+  {
+    path: '/FireRegister',
+    name: 'FireRegister',
+    component: FirebaseRegisterView,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+// router.beforeEach((to, from, next) => {
+//   if (!store.state.isAuthenticated && to.name !== 'Login') {
+//     next({ name: 'Login' });
+//   } else {
+//     next();
+//   }
+// })
+
+// router.beforeEach(async (to, from) => {
+//   if (to.name == "About"){
+//     return false;
+//   }
+// })
+
+export default router;
