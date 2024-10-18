db096b5b1c657e5677b50782b734c8d694c10272
Venture <whenchange.shi@gmail.com>
2024-10-18 16:24:21 +1100

    fix bugs

--- week-10/vite.config.js
+++ week-10/vite.config.js
@@ -18,7 +18,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // })
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/VueProjects/',
   build: {
     outDir: 'dist'
   },
