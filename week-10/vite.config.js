fa514883a1d4a4194c30f5e5e44e0b673fdd201c
Venture <whenchange.shi@gmail.com>
2024-10-18 15:00:51 +1100

    change position of yml file

--- week-10/vite.config.js
+++ week-10/vite.config.js
@@ -5,7 +5,23 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     vue(),
     vueDevTools(),
